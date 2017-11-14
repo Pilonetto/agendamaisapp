aa45d7efeb2036f7b832448ac24b1c1777533d03
Pilonetto <marlon.pilonetto@gmail.com>
2017-11-14 17:39:34 -0200

    primeiro commit

---
+++ Source/Source/www/js/config.js
@@ -0,0 +1,4 @@
+var my_key = "8656f19ba1dbf979dd36fe84e2a58ec6";
+//var coreurl = "http://192.168.138.31/TRAINEES/reshma/Bookmydoc/";
+var coreurl = "http://doesanguepatobranco.com.br/";
+var baseurl = coreurl+"Webservice";
